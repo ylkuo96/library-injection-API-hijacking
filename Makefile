9b854f3bd1935e3d1a78eac994d3b84d2fd2a515
yilin <yilin96cc@gmail.com>
2019-04-15 05:15:53 -0700

    add fprintf & fscanf

--- Makefile
+++ Makefile
@@ -2,7 +2,12 @@ hw2.so: hw2.cpp
 	g++ -o hw2.so -shared -fPIC hw2.cpp -ldl
 
 test: hw2.so
-	MONITOR_OUTPUT=fsmon.log LD_PRELOAD=./hw2.so ../testcases/file
+	#MONITOR_OUTPUT=fsmon.log LD_PRELOAD=./hw2.so ../testcases/file
+	#MONITOR_OUTPUT=fsmon.log LD_PRELOAD=./hw2.so ../testcases/dir
+	#MONITOR_OUTPUT=fsmon.log LD_PRELOAD=./hw2.so ../testcases/fscanf
+	#MONITOR_OUTPUT=fsmon.log LD_PRELOAD=./hw2.so ../testcases/getline
+	#MONITOR_OUTPUT=fsmon.log LD_PRELOAD=./hw2.so ../testcases/stdio
+	MONITOR_OUTPUT=fsmon.log LD_PRELOAD=./hw2.so ls
 
 tst: tst.cpp
 	g++ -o tst -Wall -g tst.cpp
--- hw2.cpp
+++ hw2.cpp
@@ -10,19 +10,20 @@
 #include <sstream>
 #include <unistd.h>
 #include <map>
+#include <stdarg.h>
 
 using namespace std;
 
 //map<DIR *, const char *> dirName;
-
 /*(v) closedir opendir readdir chdir
  *(v) creat open read write 
  *(v) dup dup2 close lstat stat pwrite 
- *(v) fopen fclose fread fwrite fgetc fgets
+ *(v) fread fwrite fgetc fgets
  *(v) readlink link unlink symlink
  *(v) mkdir rmdir
  *(v) chown chmod remove rename 
- *( ) fscanf fprintf 
+ *(v) fprintf fscanf 
+ *( ) fopen fclose 
  *(v) fputs_unlocked fwrite_unlocked fflush fflush_unlocked
  */
 
@@ -42,8 +43,8 @@ static size_t (*libc_fread) (void *, size_t, size_t, FILE *) = NULL;
 static size_t (*libc_fwrite) (const void *, size_t, size_t, FILE *) = NULL;
 static int (*libc_fgetc) (FILE *) = NULL;
 static char * (*libc_fgets) (char *, int, FILE *) = NULL;
-//static int (*libc_fscanf) (FILE *, const char *, ...) = NULL;
-//static int (*libc_fprintf) (FILE *, const char *, ...) = NULL;
+static int (*libc_fscanf) (FILE *, const char *, ...) = NULL;
+static int (*libc_fprintf) (FILE *, const char *, ...) = NULL;
 static DIR * (*libc_opendir) (const char *) = NULL;
 static int (*libc_closedir) (DIR *) = NULL;
 static struct dirent * (*libc_readdir) (DIR *) = NULL;
@@ -62,15 +63,15 @@ static size_t (*libc_fwrite_unlocked) (const void *, size_t, size_t, FILE *) = N
 static int (*libc_fputs_unlocked) (const char *, FILE *) = NULL;
 static int (*libc_fflush) (FILE *) = NULL;
 static int (*libc_fflush_unlocked) (FILE *) = NULL;
+void *handle = NULL;
 
 __attribute__((constructor)) int start() {
     char *filename = getenv("MONITOR_OUTPUT");
     if(filename != NULL){
         freopen(filename, "w", stderr);
     }
-
-    void *handle = dlopen("libc.so.6", RTLD_LAZY);
     
+    handle = dlopen("libc.so.6", RTLD_LAZY);
     if(handle == NULL){
         printf("error\n");
         return -1;
@@ -115,11 +116,11 @@ __attribute__((constructor)) int start() {
     if(libc_pwrite == NULL){
         libc_pwrite = (ssize_t (*) (int, const void *, size_t, off_t))dlsym(handle, "pwrite");
     }
-
+    
     if(libc_fopen == NULL){
         libc_fopen = (FILE * (*) (const char *, const char *))dlsym(handle, "fopen");
     }
-    
+
     if(libc_fclose == NULL){
         libc_fclose = (int (*) (FILE *))dlsym(handle, "fclose");
     }
@@ -140,14 +141,13 @@ __attribute__((constructor)) int start() {
         libc_fgets = (char * (*) (char *, int, FILE *))dlsym(handle, "fgets");
     }
     
-    /*
     if(libc_fscanf == NULL){
-        libc_fscanf = (int (*) (FILE *, const char *, ...))dlsym(handle, "__isoc99_fscanf");
+        libc_fscanf = (int (*) (FILE *, const char *, ...))dlsym(handle, "vfscanf");
     }
+    
     if(libc_fprintf == NULL){
-        libc_fprintf = (int (*) (FILE *, const char *, ...))dlsym(handle, "fprintf");
+        libc_fprintf = (int (*) (FILE *, const char *, ...))dlsym(handle, "vfprintf");
     }
-    */
     
     if(libc_closedir == NULL){
         libc_closedir = (int (*) (DIR *))dlsym(handle, "closedir");
@@ -226,6 +226,20 @@ __attribute__((destructor)) void finish() {
     //printf("bbb\n");
 }
 
+void Print(FILE *stream, const char *format, ...){
+    /*
+     * to prevent when implementing fprintf then get into infinite loop
+     */
+	int (*P) (FILE *, const char *, ...) = NULL;	
+	if(handle != NULL && P == NULL){
+		P = (int (*) (FILE *, const char *, ...))dlsym(handle, "vfprintf");
+	}
+	
+	va_list ap;
+	va_start(ap, format);
+	P(stream, format, ap);
+}
+
 string getfilename(int fd){
     if(fd == STDIN_FILENO){
         return "<STDIN>";
@@ -254,7 +268,7 @@ extern "C" int open(const char *pathname, int flags, ...){
     stringstream ss;
     ss << hex << flags;
     string res(ss.str());
-    fprintf(stderr, "open(\"%s\", 0x%s) = %d\n", target_path.c_str(), res.c_str(), fd); 
+    Print(stderr, "# open(\"%s\", 0x%s) = %d\n", target_path.c_str(), res.c_str(), fd); 
 
     return fd;
 }
@@ -262,7 +276,7 @@ extern "C" int open(const char *pathname, int flags, ...){
 extern "C" int close(int fd){
     string target_path = getfilename(fd);
     int res = libc_close(fd);
-    fprintf(stderr, "close(\"%s\") = %d\n", target_path.c_str(), res);
+    Print(stderr, "# close(\"%s\") = %d\n", target_path.c_str(), res);
 
     return res;
 }
@@ -270,7 +284,7 @@ extern "C" int close(int fd){
 extern "C" ssize_t read(int fd, void *buf, size_t count){
     string target_path = getfilename(fd);
     ssize_t res = libc_read(fd, buf, count);
-    fprintf(stderr, "read(\"%s\", %p, %zu) = %zd\n", target_path.c_str(), &buf, count, res);
+    Print(stderr, "# read(\"%s\", %p, %zu) = %zd\n", target_path.c_str(), &buf, count, res);
 
     return res;
 }
@@ -278,21 +292,21 @@ extern "C" ssize_t read(int fd, void *buf, size_t count){
 extern "C" ssize_t write(int fd, const void *buf, size_t count){
     string target_path = getfilename(fd);
     ssize_t res = libc_write(fd, buf, count);
-    fprintf(stderr, "write(\"%s\", %p, %zu) = %zd\n", target_path.c_str(), &buf, count, res);
+    Print(stderr, "# write(\"%s\", %p, %zu) = %zd\n", target_path.c_str(), &buf, count, res);
     
     return res;
 }
 
 extern "C" int dup(int oldfd){
     int res = libc_dup(oldfd);
-    fprintf(stderr, "dup(%d) = %d\n", oldfd, res);
+    Print(stderr, "# dup(%d) = %d\n", oldfd, res);
 
     return res;    
 }
 
 extern "C" int dup2(int oldfd, int newfd){
     int res = libc_dup2(oldfd, newfd);
-    fprintf(stderr, "dup(%d, %d) = %d\n", oldfd, newfd, res);
+    Print(stderr, "# dup(%d, %d) = %d\n", oldfd, newfd, res);
 
     return res;    
 }
@@ -300,7 +314,7 @@ extern "C" int dup2(int oldfd, int newfd){
 extern "C" int creat(const char *pathname, mode_t mode){
     int fd = libc_creat(pathname, mode);
     string target_path = getfilename(fd);
-    fprintf(stderr, "creat(\"%s\", %o) = %d\n", target_path.c_str(), mode, fd);
+    Print(stderr, "# creat(\"%s\", %o) = %d\n", target_path.c_str(), mode, fd);
 
     return fd;    
 }
@@ -309,7 +323,7 @@ extern "C" int __xstat(int __ver, const char *pathname, struct stat *statbuf){
     int res = libc_xstat(__ver, pathname, statbuf);
     mode_t Mode = statbuf->st_mode;
     off_t Size = statbuf->st_size;
-    fprintf(stderr, "stat(\"%s\", %p {mode=%o, size=%jd}) = %d\n", pathname, &statbuf, Mode, Size, res);
+    Print(stderr, "# stat(\"%s\", %p {mode=%o, size=%jd}) = %d\n", pathname, &statbuf, Mode, Size, res);
     
     return res;
 }
@@ -318,7 +332,7 @@ extern "C" int __lxstat(int __ver, const char *pathname, struct stat *statbuf){
     int res = libc_lxstat(__ver, pathname, statbuf);
     mode_t Mode = statbuf->st_mode;
     off_t Size = statbuf->st_size;
-    fprintf(stderr, "lstat(\"%s\", %p {mode=%o, size=%jd}) = %d\n", pathname, &statbuf, Mode, Size, res);
+    Print(stderr, "# lstat(\"%s\", %p {mode=%o, size=%jd}) = %d\n", pathname, &statbuf, Mode, Size, res);
     
     return res;
 }
@@ -326,44 +340,37 @@ extern "C" int __lxstat(int __ver, const char *pathname, struct stat *statbuf){
 extern "C" ssize_t pwrite(int fd, const void *buf, size_t count, off_t offset){
     ssize_t res = libc_pwrite(fd, buf, count, offset);
     string target_path = getfilename(fd);
-    fprintf(stderr, "pwrite(\"%s\", %p, %zu, %jd) = %zd\n", target_path.c_str(), &buf, count, offset, res);
+    Print(stderr, "# pwrite(\"%s\", %p, %zu, %jd) = %zd\n", target_path.c_str(), &buf, count, offset, res);
 
     return res;
 }
 
-/*
-extern "C" FILE *fopen(const char *pathname, const char *mode){
+FILE *fopen(const char *pathname, const char *mode){
     // defined a pointer named `res` pointed to a structure `FILE` 
     FILE *res = libc_fopen(pathname, mode);
     // define a `FILE` structure named `f` and assigned `the value of` res
     //FILE f = *res;      // &f
     // define a pointer named `ptr` pointed to a structure `FILE` and assigned `the address of` `f`
     //FILE *ptr = &f;     // %p
-
-    fprintf(stderr, "fopen(\"%s\", \"%s\") = %p\n", pathname, mode, res);
-    //cout << "fopen(\"" << pathname << "\", \"" << mode << "\") = " << res << endl;
+    Print(stderr, "# fopen(\"%s\", \"%s\") = %p\n", pathname, mode, res);
 
     return res;
 }
-*/
 
-/*
 extern "C" int fclose(FILE *stream){
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
     int res = libc_fclose(stream);
-    fprintf(stderr, "fclose(\"%s\") = %d\n", target_path.c_str(), res);
-    //cout << "fclose(\"" << target_path << "\") = " << res << endl;
+    Print(stderr, "# fclose(\"%s\") = %d\n", target_path.c_str(), res);
     
     return res;
 }
-*/
 
 extern "C" size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
     size_t res = libc_fread(ptr, size, nmemb, stream);
-    fprintf(stderr, "fread(%p, %zu, %zu, \"%s\") = %zu\n", &ptr, size, nmemb, target_path.c_str(), res);
+    Print(stderr, "# fread(%p, %zu, %zu, \"%s\") = %zu\n", &ptr, size, nmemb, target_path.c_str(), res);
     
     return res;
 }
@@ -372,7 +379,7 @@ extern "C" size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *strea
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
     size_t res = libc_fwrite(ptr, size, nmemb, stream);
-    fprintf(stderr, "fwrite(%p, %zu, %zu, \"%s\") = %zu\n", &ptr, size, nmemb, target_path.c_str(), res);
+    Print(stderr, "# fwrite(%p, %zu, %zu, \"%s\") = %zu\n", &ptr, size, nmemb, target_path.c_str(), res);
 
     return res;
 }
@@ -381,7 +388,7 @@ extern "C" int fgetc(FILE *stream){
     int res = libc_fgetc(stream);
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
-    fprintf(stderr, "fgetc(\"%s\") = %d\n", target_path.c_str(), res);
+    Print(stderr, "# fgetc(\"%s\") = %d\n", target_path.c_str(), res);
 
     return res;
 }
@@ -390,42 +397,42 @@ extern "C" char *fgets(char *s, int size, FILE *stream){
     char *res = libc_fgets(s, size, stream);
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
-    fprintf(stderr, "fgets(\"%s\", %d, \"%s\") = %s\n", s, size, target_path.c_str(), res);
+    Print(stderr, "# fgets(\"%s\", %d, \"%s\") = %s\n", s, size, target_path.c_str(), res);
 
     return res;
 }
 
-/*
-#include <stdarg.h>
 extern "C" int __isoc99_fscanf(FILE *stream, const char *format, ...){
-    void *ptr;
-
-    va_list args;
-    va_start(args, format);
-    ptr = va_arg(args, void*);
-    va_end(args);
-    int res = libc_fscanf(stream, format, ptr);
-
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
-    fprintf(stderr, "# fscanf(\"%s\", \"%s\", %p) = %d\n", target_path.c_str(), format, ptr, res); 
+    
+    va_list ap;
+    va_start(ap, format);
+    int res = libc_fscanf(stream, format, ap);
+    Print(stderr, "# fscanf(\"%s\", \"%s\", ...) = %d\n", target_path.c_str(), format, res);
+    va_end(ap);
 
-    fprintf(stderr, "%d %d", a, b);
     return res;
 }
 
-int ffprintf(stderr, FILE *stream, const char *format, ...){
-    //int res = libc_fscanf(stream, format, ...);
-    // return res;
-    return 0;
+extern "C" int fprintf(FILE *stream, const char *format, ...){
+    int fd = stream->_fileno;
+    string target_path = getfilename(fd);
+
+    va_list ap;
+    va_start(ap, format);
+    int res = libc_fprintf(stream, format, ap);
+    Print(stderr, "# fprintf(\"%s\", \"%s\", ...) = %d\n", target_path.c_str(), format, res);
+    va_end(ap);
+
+    return res;
 }
-*/
 
 extern "C" DIR * opendir(const char * name){
     DIR * res = libc_opendir(name);
     int fd = dirfd(res);
     string target_path = getfilename(fd);
-    fprintf(stderr, "opendir(\"%s\") = %p\n", target_path.c_str(), &res);
+    Print(stderr, "# opendir(\"%s\") = %p\n", target_path.c_str(), &res);
     //dirName.insert(pair<DIR *, const char *>(res, name));
 
     return res;
@@ -435,7 +442,7 @@ extern "C" int closedir(DIR *dirp){
     int fd = dirfd(dirp);
     string target_path = getfilename(fd);
     int res = libc_closedir(dirp);
-    fprintf(stderr, "closedir(\"%s\") = %d\n", target_path.c_str(), res);
+    Print(stderr, "# closedir(\"%s\") = %d\n", target_path.c_str(), res);
     //dirName.erase(dirp);
 
     return res;
@@ -445,10 +452,10 @@ extern "C" struct dirent *readdir(DIR *dirp){
     struct dirent *res = libc_readdir(dirp);
     int fd = dirfd(dirp);
     if(res == NULL){
-        fprintf(stderr, "readdir(\"%s\") = NULL\n", getfilename(fd).c_str());
+        Print(stderr, "# readdir(\"%s\") = NULL\n", getfilename(fd).c_str());
     }
     else{
-        fprintf(stderr, "readdir(\"%s\") = %s\n", getfilename(fd).c_str(), res->d_name);
+        Print(stderr, "# readdir(\"%s\") = %s\n", getfilename(fd).c_str(), res->d_name);
     }
 
     return res;
@@ -456,70 +463,70 @@ extern "C" struct dirent *readdir(DIR *dirp){
 
 extern "C" int chdir(const char *path){
     int res = libc_chdir(path);
-    fprintf(stderr, "chdir(\"%s\") = %d\n", path, res);
+    Print(stderr, "# chdir(\"%s\") = %d\n", path, res);
 
     return res;
 }
 
 extern "C" ssize_t readlink(const char *pathname, char *buf, size_t bufsize){
     ssize_t res = libc_readlink(pathname, buf, bufsize);
-    fprintf(stderr, "readlink(\"%s\", %p, %zu) = %zd\n", pathname, &buf, bufsize, res);
+    Print(stderr, "# readlink(\"%s\", %p, %zu) = %zd\n", pathname, &buf, bufsize, res);
 
     return res;
 }
 
 extern "C" int link(const char *oldpath, const char *newpath){
     int res = libc_link(oldpath, newpath);
-    fprintf(stderr, "link(\"%s\", \"%s\") = %d\n", oldpath, newpath, res);
+    Print(stderr, "# link(\"%s\", \"%s\") = %d\n", oldpath, newpath, res);
 
     return res;
 }
 
 extern "C" int unlink(const char *pathname){
     int res = libc_unlink(pathname);
-    fprintf(stderr, "unlink(\"%s\") = %d\n", pathname, res);
+    Print(stderr, "# unlink(\"%s\") = %d\n", pathname, res);
 
     return res;
 }
 
 extern "C" int symlink(const char *target, const char *linkpath){
     int res = libc_symlink(target, linkpath);
-    fprintf(stderr, "symlink(\"%s\", \"%s\") = %d\n", target, linkpath, res);
+    Print(stderr, "# symlink(\"%s\", \"%s\") = %d\n", target, linkpath, res);
 
     return res;
 }
 
 extern "C" int mkdir(const char *pathname, mode_t mode){
     int res = libc_mkdir(pathname, mode);
-    fprintf(stderr, "mkdir(\"%s\", %o) = %d\n", pathname, mode, res);
+    Print(stderr, "# mkdir(\"%s\", 0%o) = %d\n", pathname, mode, res);
 
     return res;
 }
 
 extern "C"  int rmdir(const char *pathname){
     int res = libc_rmdir(pathname);
-    fprintf(stderr, "rmdir(\"%s\") = %d\n", pathname, res);
+    Print(stderr, "# rmdir(\"%s\") = %d\n", pathname, res);
 
     return res;
 }
 
 extern "C" int chown(const char *pathname, uid_t owner, gid_t group){
     int res = libc_chown(pathname, owner, group);
-    fprintf(stderr, "chown(\"%s\", %d, %d) = %d\n", pathname, owner, group, res);
+    Print(stderr, "# chown(\"%s\", %d, %d) = %d\n", pathname, owner, group, res);
 
     return res;
 }
 
 extern "C" int chmod(const char *pathname, mode_t mode){
     int res = libc_chmod(pathname, mode);
-    fprintf(stderr, "chmod(\"%s\", %o) = %d\n", pathname, mode, res);
+    Print(stderr, "# chmod(\"%s\", 0%o) = %d\n", pathname, mode, res);
 
     return res;
 }
 
 extern "C" int remove(const char *pathname){
     int res = libc_remove(pathname);
-    fprintf(stderr, "remove(\"%s\") = %d\n", pathname, res);
+    Print(stderr, "# remove(\"%s\") = %d\n", pathname, res);
 
     return res;
 
@@ -527,7 +534,7 @@ extern "C" int remove(const char *pathname){
 
 extern "C" int rename(const char *oldpath, const char *newpath){
     int res = libc_rename(oldpath, newpath);
-    fprintf(stderr, "rename(\"%s\", \"%s\") = %d\n", oldpath, newpath, res);
+    Print(stderr, "# rename(\"%s\", \"%s\") = %d\n", oldpath, newpath, res);
 
     return res;
 }
@@ -536,7 +543,7 @@ extern "C" size_t fwrite_unlocked(const void *ptr, size_t size, size_t n, FILE *
     size_t res = libc_fwrite_unlocked(ptr, size, n, stream);
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
-    fprintf(stderr, "fwrite_unlocked(%p, %zu, %zu, \"%s\") = %zu\n", &ptr, size, n, target_path.c_str(), res);
+    Print(stderr, "# fwrite_unlocked(%p, %zu, %zu, \"%s\") = %zu\n", &ptr, size, n, target_path.c_str(), res);
 
     return res;
 }
@@ -545,7 +552,7 @@ extern "C" int fputs_unlocked(const char *s, FILE *stream){
     int res = libc_fputs_unlocked(s, stream);
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
-    fprintf(stderr, "fputs_unlocked(%p, \"%s\") = %d\n", &s, target_path.c_str(), res);
+    Print(stderr, "# fputs_unlocked(%p, \"%s\") = %d\n", &s, target_path.c_str(), res);
     
     return res;
 }
@@ -554,7 +561,7 @@ extern "C" int fflush(FILE *stream){
     int res = libc_fflush(stream);
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
-    fprintf(stderr, "fflush(\"%s\") = %d\n", target_path.c_str(), res);
+    Print(stderr, "# fflush(\"%s\") = %d\n", target_path.c_str(), res);
 
     return res;
 }
@@ -563,7 +570,7 @@ extern "C" int fflush_unlocked(FILE *stream){
     int res = libc_fflush_unlocked(stream);
     int fd = stream->_fileno;
     string target_path = getfilename(fd);
-    fprintf(stderr, "fflush_unlocked(\"%s\") = %d\n", target_path.c_str(), res);
+    Print(stderr, "# fflush_unlocked(\"%s\") = %d\n", target_path.c_str(), res);
 
     return res;
 }
